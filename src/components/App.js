da1a740cc2076ed309ae291413739fad4eceb96d
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-08-27 21:50:39 +0530

    submitting code-Sun Aug 27 2023 21:50:37 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React,{useContext} from "react";
 import './../styles/App.css';
-
+import global from "../context/globalcontext";
 const App = () => {
+  let {sum,set}= useContext(global)
+  console.log(sum)
+  function adddata(event){
+    console.log(event)
+    set([...sum,Number(event.target.value)])
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+        <input type='number' onChange={adddata}></input>
+         <p>Sum: {
+          sum.reduce((accu,element)=>accu+element,0)
+          }
+          </p>
     </div>
   )
 }
